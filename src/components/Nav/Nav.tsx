74b7bc86d3a8c6aa29221e7cebe9a55ce5b2619a
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-26 12:39:54 -0700

    Create social and resume icons

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,18 @@
 import './Nav.css';
 
+const linkedIn = require('../../assets/linkedin.png');
+const gitHub = require('../../assets/github.png');
+const resume = require('../../assets/resume.png');
+
 function Nav() {
   return (
     <nav className="navigation">
       <div>Home | About | Projects | Skills | Contact</div>
-      <div>LinkedIn | GitHub | Resume | Light/Dark</div>
+      <div className="icons">
+        <a href="https://www.linkedin.com/in/victoria-ashley-fields/" target='_blank' rel='noopener'><img src={linkedIn} alt="LinkedIn icon"/></a>
+        <a href="https://github.com/vfields" target='_blank' rel='noopener'><img src={gitHub} alt="GitHub icon"/></a>
+        <a href="/" target='_blank' rel='noopener'><img src={resume} alt="Resume icon"/></a>
+      </div>
     </nav>
   );
 }
