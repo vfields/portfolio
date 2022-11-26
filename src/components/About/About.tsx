503493ec125d4a0d63bbf8fa152cf3b7a5e94ad7
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-26 14:05:48 -0700

    Create rough About

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,12 +1,17 @@
 import './About.css';
 
+const fields = require('../../assets/fields.png');
+
 function About() {
   return (
-    <section>
-      <p>A sentence about your previous work.</p>
-      <p>A sentence about Turing, "earning a certificate from one of two nationally accredited coding bootcamps."
-        "I quickly absorbed new concepts, while bringing ambitious ideas and a positive attitude to every challenge..." sentence</p>
-      <p>When I'm not coding... sentence.</p>
+    <section className="about">
+      <div className="about-text">
+        <p>Before software, I was a pelvic physical therapist and small business owner, providing compassionate and trauma-informed care to patients of all ages, abilities, and backgrounds. </p>
+        <p>In 2022, after recovering from a significant pelvic injury of my own—of all things!—I took a leap and enrolled in the nationally accredited front-end engineering program at the <a href="https://turing.edu/" target='_blank' rel='noopener'>Turing School of Software & Design</a>.
+          I quickly fell in love with the problem-solving, collaboration, and creativity that programming offers, and I'm so looking forward to the next chapter.</p>
+        <p>When I'm not coding, you'll find me adventuring with my husband and pupflake, writing, or spending time outside.</p>
+      </div>
+      <img src={fields} alt="Tori, smiling and wearing a burnt orange velvet sweater." />
     </section>
   );
 }
