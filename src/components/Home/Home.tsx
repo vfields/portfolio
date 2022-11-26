28ae6386074944cba08afd1df11596c7246faaf8
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-26 11:46:19 -0700

    Complete landing page style sans links

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,9 +3,9 @@ import './Home.scss';
 function Home() {
   return (
     <section>
-      <h1 className="intro">Hi, I'm Tori.</h1>
-      <h1 className="intro engineer">I'm a software engineer.</h1>
-      <p>Maybe some sentence with invitations to click things...</p>
+      <h1 className="name">Hi, I'm Tori.</h1>
+      <h1 className="name engineer">I'm a software engineer.</h1>
+      <div className="intro">Nice to meet you! Check out my projects, resume, or contact me if you'd like to get in touch.</div>
     </section>
   );
 }
