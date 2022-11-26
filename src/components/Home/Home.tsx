09f59cf6b890893d9dbba8e72f3ee3fc7e7b518c
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-26 11:26:40 -0700

    Import sass, create home page animation

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,10 @@
-import './Home.css';
+import './Home.scss';
 
 function Home() {
   return (
     <section>
       <h1 className="intro">Hi, I'm Tori.</h1>
-      <h1 className="intro">I'm a <span className="name">software engineer</span>.</h1>
+      <h1 className="intro engineer">I'm a software engineer.</h1>
       <p>Maybe some sentence with invitations to click things...</p>
     </section>
   );
