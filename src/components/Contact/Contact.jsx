58dfabef8251462e8f59c4096e3aef79704407ed
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-29 10:05:51 -0700

    Add state to form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,11 +6,14 @@
 
 // export default Contact;
 
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import './Contact.css';
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -27,12 +30,26 @@ function Contact() {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input 
+        type="text" 
+        name="user_name"
+        value={name}
+        onChange={(event) => setName(event.target.value)} 
+      />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input 
+        type="email" 
+        name="user_email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)} 
+      />
       <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+      <textarea 
+        name="message"
+        value={message}
+        onChange={(event) => setMessage(event.target.value)} 
+      />
+      <button type="submit">Submit</button>
     </form>
   );
 };
