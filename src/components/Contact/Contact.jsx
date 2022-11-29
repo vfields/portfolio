993f494d4b6976e572cf015c419aea502cb1a310
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-29 11:09:28 -0700

    Add error handling to form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,3 @@
-// function Contact() {
-//   return (
-//     <p>I am Contact</p>
-//   );
-// }
-
-// export default Contact;
-
 import { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import './Contact.css';
@@ -14,42 +6,66 @@ function Contact() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('service_sdg36no', 'template_ksuhawl', form.current, 'i3aDurtlbfViXugse')
+    if (name && email && message) {
+      emailjs.sendForm('service_sdg36no', 'template_ksuhawl', form.current, 'i3aDurtlbfViXugse')
       .then((result) => {
-          console.log(result.text);
+        setError('');
+        setSuccess('Your message was sent successfully!');
+        clearForm();
       }, (error) => {
-          console.log(error.text);
+        setError(`Uh oh! Something went wrong sending your message: ${error.text}. Please refresh and try again!`);
       });
+    } else {
+      setError('Oops! Please be sure to fill out all inputs before submitting.');
+    }
   };
+
+  const clearForm = () => {
+    setName('');
+    setEmail('');
+    setMessage('');
+  }
+
+  const clearMsg = () => {
+    setSuccess('');
+  }
 
   return (
     <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
       <input 
         type="text" 
         name="user_name"
+        placeholder="Name"
         value={name}
         onChange={(event) => setName(event.target.value)} 
       />
-      <label>Email</label>
       <input 
         type="email" 
         name="user_email"
+        placeholder="Email"
         value={email}
         onChange={(event) => setEmail(event.target.value)} 
       />
-      <label>Message</label>
       <textarea 
         name="message"
+        placeholder="Message"
         value={message}
         onChange={(event) => setMessage(event.target.value)} 
       />
-      <button type="submit">Submit</button>
+      <button type="submit">Send Message</button>
+      {error && <p className="user-msg">{error}</p>}
+      {success && 
+        <>
+          <p className="user-msg">{success}</p>
+          <button onClick={clearMsg}>OK</button>
+        </>
+      }
     </form>
   );
 };
