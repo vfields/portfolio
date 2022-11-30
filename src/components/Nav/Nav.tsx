890a9e1a7304a455a473301c86d81376e5b36230
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-30 10:26:45 -0700

    Add navlink styling

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 const linkedIn = require('../../assets/linkedin.png');
@@ -8,7 +9,7 @@ function Nav() {
   return (
     <nav className="navigation">
       <div className="links">
-        Home About Projects Skills Contact
+      <NavLink exact to='/' className='inactive'>Home</NavLink> <NavLink exact to='/about' className='inactive'>About</NavLink> <NavLink exact to='/projects' className='inactive home'>Projects</NavLink> <NavLink exact to='/skills' className='inactive'>Skills</NavLink> <NavLink exact to='/contact' className='inactive'>Contact</NavLink>
       </div>
       <div className="icons">
         <a href="https://www.linkedin.com/in/victoria-ashley-fields/" target='_blank' rel='noopener'><img src={linkedIn} alt="LinkedIn icon"/></a>
