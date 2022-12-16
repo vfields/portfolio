f3aed828a0fefbebaf135d51144756bb7a047622
Victoria Fields <victoriaashleyfields@gmail.com>
2022-12-15 18:27:29 -0700

    Add eat local project card

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,6 +3,7 @@ import './Projects.css';
 const halfFlower = require('../../assets/half-flower.png');
 const rHalfFlower = require('../../assets/r-half-flower.png');
 const findmyapi = require('../../assets/findmyapi.png');
+const eatlocal = require('../../assets/eatlocal.png');
 const rancid = require('../../assets/rancid.png');
 const fitlit = require('../../assets/fitlit.png');
 
@@ -20,8 +21,20 @@ function Projects() {
         <div className="project-text">
           <h4>Solo | 5 days</h4>
           <p className="description">This responsive and accessible application allows users to search over 1,000 free and public APIs by keyword, category, authentication requirements, CORs policies, and more. Users can save APIs of interest while they continue their search, or take a break with adorable dog pictures and videos!</p>
-          <p className="tech-list">TypeScript, React, React Hooks, HTML/JSX, Cypress, CSS, Git, Accessibility, Responsive Design, Webpack, Node.js</p>
+          <p className="tech-list">TypeScript, React, React Hooks, HTML/JSX, Cypress, CSS, Git, Accessibility, Responsive Design, Webpack</p>
           <span className="links"><a href="https://find-my-api.vercel.app/" target='_blank' rel='noopener'>Live Preview</a> | <a href="https://github.com/vfields/find-my-api" target='_blank' rel='noopener'>Code Base</a></span>
+        </div>
+      </article>
+      <article className="project">
+        <div className="title-img-container">
+          <h3 className="title">Eat Local</h3>
+          <img src={eatlocal} alt="preview of Eat Local's search and login features"/>
+        </div>
+        <div className="project-text">
+          <h4>Group | 14 days</h4>
+          <p className="description">This accessible application was built on a fullstack team of 7 engineers—3 FE, 4 BE—featuring GitHub's issue ticketing system and CI/CD. It allows any user to search for local restaurants, markets, and breweries, and a logged in user to store and delete their favorites!</p>
+          <p className="tech-list">GraphQL, CircleCI, React, React Hooks, JavaScript, HTML/JSX, Cypress, CSS, Git, Accessibility, Webpack</p>
+          <span className="links"><a href="https://eatlocal.vercel.app/" target='_blank' rel='noopener'>Live Preview</a> | <a href="https://github.com/Eat-Local/eat-local-fe" target='_blank' rel='noopener'>Code Base</a></span>
         </div>
       </article>
       <article className="project">
@@ -32,7 +45,7 @@ function Projects() {
         <div className="project-text">
           <h4>Paired | 6 days</h4>
           <p className="description">This accessible application displays a database of movies, and allows users to search for their favorites by title! Clicking a film will redirect the user to a page that displays that movie's unique information, and grants access to the movie's trailer!</p>
-          <p className="tech-list">React, JavaScript, HTML/JSX, Cypress, CSS, RESTful APIs, Git, Accessibility, Webpack, Node.js</p>
+          <p className="tech-list">React, React Router, JavaScript, HTML/JSX, Cypress, CSS, RESTful APIs, Git, Accessibility, Webpack</p>
           <span className="links"><a href="https://rancid-tomatillos-theta.vercel.app/" target='_blank' rel='noopener'>Live Preview</a> | <a href="https://github.com/vfields/rancid-tomatillos" target='_blank' rel='noopener'>Code Base</a></span>
         </div>
       </article>
